f964478f053904bcbf170a5537af035d0e31f53b
abadami <andrewbadami@gmail.com>
2023-06-08 21:18:11 -0500

    convert_time_to_seconds

--- src/main.rs
+++ src/main.rs
@@ -23,18 +23,68 @@ fn get_race_titles_for_page_number(
     Ok(())
 }
 
-//TODO: Figure out how to parse page number
+fn get_fatest_time_for_race(title: String) -> Result<usize, Box<dyn std::error::Error>> {
+    let mut base_url = "https://racetime.gg/pm64r/".to_string();
+
+    base_url.push_str(&title);
+
+    let response = reqwest::blocking::get(base_url)?.text()?;
+
+    let document = Html::parse_document(&response);
+    let selector = Selector::parse("time.finish-time")?;
+
+    let finish_time_node = document.select(&selector).next().unwrap();
+
+    let finish_time = finish_time_node.text().collect::<String>();
+
+    Ok(0)
+}
+
+fn convert_time_to_seconds(time: String) -> usize {
+    let mut total_time = 0;
+
+    let time_split = time.split(':');
+
+    let mut multiplier = 3600;
+
+    for time in time_split {
+        let parsed_time = time.parse::<usize>();
+
+        let converted_seconds = match parsed_time {
+            Ok(val) => val * multiplier,
+            _ => 0,
+        };
+
+        total_time += converted_seconds;
+        multiplier /= 60;
+    }
+
+    total_time
+}
+
 //TODO: Figure out how to traverse each page
 //TODO: Figure out how to fetch each detail page
 //TODO: Figure out STATISTICS
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut races = HashMap::<String, usize>::new();
 
-    get_race_titles_for_page_number(&mut races, 1)?;
+    let mut previous_length = 0;
+    let mut current_length = 1;
+    let mut counter = 1;
+
+    while current_length > previous_length {
+        previous_length = current_length;
+
+        get_race_titles_for_page_number(&mut races, counter)?;
+
+        counter += 1;
+        current_length = races.len();
+    }
 
     let race_title = races.iter().next().unwrap().0;
 
     println!("Race Title: {}", race_title);
+    println!("Races Length: {}", races.len());
 
     let mut race_details_url: String = "https://racetime.gg/pm64r/".to_string();
 
@@ -53,16 +103,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let finish_time = finish_time_node.text().collect::<String>();
 
     println!("Finish Time: {}", finish_time);
-
-    /*
-    for element in document.select(&selector) {
-        let race_title = element.text().collect::<String>();
-
-
-
-        println!("{}", element.text().collect::<String>());
-    }
-    */
 
     Ok(())
 }
