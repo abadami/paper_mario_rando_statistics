2eb2ffccd3175ed518ac820dc3062710021892ac
abadami <andrewbadami@gmail.com>
2025-05-26 10:42:54 -0500

    Some starting components

---
+++ client/src/components/OptionSelect.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <select>
+    <option value="option1">Option 1</option>
+    <option value="option2">Option 2</option>
+    <option value="option3">Option 3</option>
+  </select>
+</template>
+
+<style></style>
---
+++ client/src/components/StatisticBlock.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  title?: string
+  value: number | string
+}>()
+</script>
+
+<template>
+  <div class="statistic-block">
+    <h4 class="statistic-header">{{ title }}</h4>
+    <p class="statistic-value">{{ value }}</p>
+  </div>
+</template>
+
+<style></style>
