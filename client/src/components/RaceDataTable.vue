30ae569e48a2639823368d32752a38e31d1bdd39
SalamiTsunami <andrewbadami@gmail.com>
2025-06-19 16:44:09 -0500

    Quick race data table implementation

---
+++ client/src/components/RaceDataTable.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+defineProps({})
+</script>
+
+<template>
+  <table>
+    <thead>
+      <th>Race Date</th>
+      <th>Race Name</th>
+      <th>Race Placement</th>
+      <th>Finish Time</th>
+    </thead>
+    <tbody>
+      <td>05-12-2025</td>
+      <td>Jolly-Kolorado</td>
+      <td>2nd</td>
+      <td>01:46:34.34</td>
+    </tbody>
+  </table>
+</template>
+
+<style></style>
