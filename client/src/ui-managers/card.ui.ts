992dca61fd77b6bc24b697a6b7cc032764cc37ba
SalamiTsunami <andrewbadami@gmail.com>
2025-07-29 22:47:23 -0500

    Small adjustments

--- client/src/ui-managers/card.ui.ts
+++ client/src/ui-managers/card.ui.ts
@@ -5,34 +5,3 @@ export function updateCard(elementId: string, value: string) {
 
   element.textContent = value;
 }
-
-export function updateAverageCard(value: string) {
-  const element = document.querySelector<HTMLSpanElement>("#average-value")!;
-
-  element.textContent = value;
-}
-
-export function updateDeviationCard(value: string) {
-  const element = document.querySelector<HTMLSpanElement>("#deviation-value")!;
-
-  element.textContent = value;
-}
-
-export function updateAverageWinCard(value: string) {
-  const element =
-    document.querySelector<HTMLSpanElement>("#average-win-value")!;
-
-  element.textContent = value;
-}
-
-export function updateBestWinCard(value: string) {
-  const element = document.querySelector<HTMLSpanElement>("#best-win-value")!;
-
-  element.textContent = value;
-}
-
-export function updateWorstLossCard(value: string) {
-  const element = document.querySelector<HTMLSpanElement>("#worst-loss-value")!;
-
-  element.textContent = value;
-}
